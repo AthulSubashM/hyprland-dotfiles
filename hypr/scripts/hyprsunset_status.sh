74e330cb261fadddf53d23a98a1199b1b70c65d7
AthulSubashM <athul.m.subash@gmail.com>
2025-10-14 02:06:07 -0400

    deleting root files

--- hypr/scripts/hyprsunset_status.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Check if the process or the state file exists
-if pgrep -f "hyprsunset_toggle.sh --active" > /dev/null || [ -f "/tmp/hyprsunset_is_on" ]; then
-  echo "true"
-else
-  echo "false"
-fi
